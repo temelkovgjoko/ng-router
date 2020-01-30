a43f1979e1cd62bf978788a1e15ea96887519d5e
Gjoko <temelkovgjoko@gmail.com>
2020-01-30 16:52:21 +0100

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroesModule } from './heroes/heroes.module';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, CrisisListComponent, PageNotFoundComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HeroesModule,
+    AppRoutingModule,
+    // BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+import { HeroesRoutingModule } from "./heroes-routing.module";
+import { FormsModule } from "@angular/forms";
+import { HeroListComponent } from "./hero-list/hero-list.component";
+import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
+
+@NgModule({
+  declarations: [HeroListComponent, HeroDetailComponent],
+  imports: [CommonModule, HeroesRoutingModule, FormsModule]
+})
+export class HeroesModule {}
